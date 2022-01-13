b50552604151e30d1c79b4990d897f821d8920f2
Jillian Anderson <willfrog1224@yahoo.com>
2022-01-12 19:37:22 -0800

    update

--- routes/ta02.js
+++ routes/ta02.js
@@ -2,14 +2,53 @@
 // Remember, you can make more of these placeholders yourself!
 const express = require('express');
 const router = express.Router();
+let error = '';
+const users = [
+  {
+    name: "Bob Rob"
+  },
+  {
+    name: "Marge Simpson"
+  },
+  {
+    name: "Bart Simpson"
+  }
+];
+
+router.post('/addUser', (req, res, next) => {
+  const name = req.body.name;
+  const userIndex = users.findIndex((user) => user.name === name);
+  if (userIndex >= 0){
+    error = 'User Already Exists';
+  } 
+  else { 
+    users.push({name});
+  }
+  res.redirect("/ta02");
+});
+
+router.post('/removeUser', (req, res, next) => {
+  const name = req.body.name;
+  const userIndex = users.findIndex((user) => user.name === name);
+  if (userIndex >= 0){
+    users.splice(userIndex, 1);
+  } 
+  else{
+    error = "User Not Found"
+  }
+  res.redirect("/ta02");
+});
 
 router.get('/', (req, res, next) => {
-  res.render('pages', {
+  res.render('pages/ta02', {
     title: 'Team Activity 02',
     path: '/ta02', // For pug, EJS
-    activeTA03: true, // For HBS
+    activeTA03: true, // For HBS    
     contentCSS: true, // For HBS
+    users: users,
+    error: error
   });
+  error = '';
 });
 
 module.exports = router;
