755122f8f6bcb9ca575fe8ae794d10a35696478d
Jillian Anderson <willfrog1224@yahoo.com>
2022-01-19 19:11:18 -0800

    update

--- routes/ta03.js
+++ routes/ta03.js
@@ -2,13 +2,33 @@
 const express = require('express');
 const router = express.Router();
 
+const fs = require('fs');
+const path = require('path');
+
 router.get('/', (req, res, next) => {
-  res.render('pages/ta03', {
-    title: 'Team Activity 03',
-    path: '/ta03', // For pug, EJS
-    activeTA03: true, // For HBS
-    contentCSS: true, // For HBS
+  const p = path.join(__dirname, '..', 'data', 'products.json');
+  fs.readFile(p, (err, content) =>{
+
+    if (err) {
+      conlog.log(err);
+    }
+    let products = JSON.parse(content);
+    const criteria = req.query.criteria;
+    if(criteria){
+      products = products.filter(
+        (product) => product.name.search(criteria) >= 0
+        );
+    }
+
+    res.render('pages/ta03', {
+      title: 'Team Activity 03',
+      path: '/ta03', // For pug, EJS
+      activeTA03: true, // For HBS
+      contentCSS: true, // For HBS
+    });
+
   });
+
 });
 
 module.exports = router;
